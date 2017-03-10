5e2718740a818b5f0b9c8101321f934dd18438ef
Ievgen Krasnyi <ievgen.krasnyi@gmail.com>
2017-03-10 16:44:21 +0200

    added back button

--- src/views/CityForecastView.js
+++ src/views/CityForecastView.js
@@ -1,8 +1,18 @@
 import React from 'react'
 
 export default React.createClass({
+    contextTypes: {
+        router: React.PropTypes.object
+    },
+
+    navigateToCitySelector(e) {
+        e.preventDefault();
+        this.context.router.push('/');
+    },
+
     render() {
         return <div>
+            <button type="button" onClick={this.navigateToCitySelector}>Back</button>
             <div>{this.props.params.cityName} Forecast</div>
         </div>
     }
