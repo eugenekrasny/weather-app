ec360f98c84ba64b385c8818b521f1f32e3ba2de
Ievgen Krasnyi <ievgen.krasnyi@gmail.com>
2017-03-11 11:17:52 +0200

    added api calls and data filtering

--- src/views/CityForecastView.js
+++ src/views/CityForecastView.js
@@ -1,19 +1,84 @@
 import React from 'react'
+import moment from 'moment'
 
-export default React.createClass({
-    contextTypes: {
-        router: React.PropTypes.object
-    },
+class CityForecastView extends React.Component {
+
+    constructor(props) {
+        super(props);
+        const params = props.params;
+        let coords = null;
+        if (params.lon && params.lat) {
+            coords = {
+                lon: params.lon,
+                lat: params.lat
+            };
+        }
+        this.state = {
+            cityName: params.cityName,
+            coords: coords
+        };
+        this.navigateToCitySelector = this.navigateToCitySelector.bind(this);
+    }
 
     navigateToCitySelector(e) {
         e.preventDefault();
         this.context.router.push('/');
-    },
+    }
+
+    componentWillMount() {
+        let apiRoute = null;
+        if (this.state.coords) {
+            const coords = this.state.coords;
+            apiRoute = '/forecast?lat=' + coords.lat + '&lon=' + coords.lon + '&appid=ac4ac652654d00e02b9cbe592d3848ce';
+        } else if (this.state.cityName) {
+            apiRoute = '/forecast?q=' + this.state.cityName + '&appid=ac4ac652654d00e02b9cbe592d3848ce';
+        }
+        fetch(apiRoute)
+            .then(response => {
+                return response.json();
+            })
+            .then(json => {
+                if (200 === parseInt(json.cod, 10)) {
+                    const todaysEndOfDay = moment.utc().endOf('day').unix();
+                    json.list = json.list.filter(hourlyForecast => {
+                        const hourlyForecastDate = moment.unix(hourlyForecast.dt).utc();
+                        const hourlyForecastStartOfDayDate = moment.unix(hourlyForecast.dt).utc().startOf('day');
+                        const diffInHours = hourlyForecastDate.diff(hourlyForecastStartOfDayDate, 'hours');
+
+                        return hourlyForecast.dt < todaysEndOfDay || (hourlyForecast.dt > todaysEndOfDay && 12 === diffInHours);
+                    });
+                    json.cnt = json.list.length;
+                    this.setState({
+                        forecast: json
+                    });
+                } else {
+                    this.setState({
+                        forecastError: json
+                    });
+                }
+                console.log(json);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
 
     render() {
+        let cityName = null;
+        if (this.state && this.state.forecast) {
+            const forecast = this.state.forecast;
+            cityName = forecast.city.name;
+        } else {
+            cityName = this.state.cityName;
+        }
         return <div>
-            <button type="button" onClick={this.navigateToCitySelector}>Back</button>
-            <div>{this.props.params.cityName} Forecast</div>
+            <button type="button" onClick={this.navigateToCitySelector}>Back</button> <span>{cityName}</span>
         </div>
     }
-})
+}
+
+CityForecastView.contextTypes = {
+    router: React.PropTypes.object
+};
+
+export default CityForecastView;
--- src/views/CitySelectionView.js
+++ src/views/CitySelectionView.js
@@ -4,8 +4,10 @@ import { Link } from 'react-router'
 export default React.createClass({
     render() {
         return <div>
-            <div>City Selection</div>
-            <div><Link to="/forecast/Tallin">Tallin</Link></div>
+            <div><Link to="/Tallinn">Tallinn</Link></div>
+            <div><Link to="/Mykolayiv">Mykolayiv</Link></div>
+            <div><Link to="/47/32">Mykolayiv coords</Link></div>
+            <div><Link to="/1111111">Error City</Link></div>
         </div>
     }
 })
