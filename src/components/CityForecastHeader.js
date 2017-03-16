67fde3eee2fd5e138bae9ae42b9e93d69fd3c93b
Ievgen Krasnyi <ievgen.krasnyi@gmail.com>
2017-03-16 22:15:52 +0200

    removing divs for cityForecastHeader

--- src/components/CityForecastHeader.js
+++ src/components/CityForecastHeader.js
@@ -3,9 +3,9 @@ import '../css/cityForecastHeader.css'
 
 class CityForecastHeader extends React.Component {
     render() {
-        return <div className="header">
+        return <h2 className="header">
             <button type="button" className="button-back" onClick={this.props.onBackClickHandler}><i className="material-icons">&#xE5C4;</i></button> {this.props.cityName}
-        </div>
+        </h2>
     }
 }
 
