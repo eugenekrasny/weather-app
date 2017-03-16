49b19860481afb392e7c6af2b7a7f74885b2d170
Ievgen Krasnyi <ievgen.krasnyi@gmail.com>
2017-03-16 21:46:03 +0200

    replaced ‘i’ tag with ‘button’

--- src/components/CityForecastHeader.js
+++ src/components/CityForecastHeader.js
@@ -4,7 +4,7 @@ import '../css/cityForecastHeader.css'
 class CityForecastHeader extends React.Component {
     render() {
         return <div className="header">
-            <i className="button-back material-icons" onClick={this.props.onBackClickHandler}>&#xE5C4;</i> <span>{this.props.cityName}</span>
+            <button type="button" className="button-back" onClick={this.props.onBackClickHandler}><i className="material-icons">&#xE5C4;</i></button> {this.props.cityName}
         </div>
     }
 }
